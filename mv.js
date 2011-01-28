48b6b398284f2ec58503c4121d1f5fc29c24d9f4
Marcus Phillips <github.com@marcusphillips.com>
2011-01-28 03:50:48 -0800

    loops and recursive parent traversal working

--- mv.js
+++ mv.js
@@ -17,23 +17,27 @@
 
     /*
      * when a command runs, it will have a 'this' scope like the following (arrows indicate prototype relationships
-     * mv.commands {
-     *   command handler definitions
-     * }
-     *  ^
-     *  |
-     * mv.commandScope {
+     *
+     * mv {
      *   lookup(key)
+     *   commands {
+     *     command handler definitions
+     *   }
      * }
+     *
      *  ^
      *  |
      * updateScope {
-     *   _rootNode, _rootScope
-     *   scopeChainCache
+     *   root
+     *   baseScopeChain
+     *   nodes to be processed
+     *   scopeChains
      * }
+     *
      *  ^
      *  |
      * processingScope {
+     *   node
      *   scopeChain
      * }
      */
@@ -41,28 +45,76 @@
     commands: {
 
       within: function(key){
-        this.scopeChain.push(this._lookup(key));
+        this.scopeChain.push(this.lookup(key));
       },
 
       without: function(key){
+        // todo: make sure this can't pop pseudoscopes
+        //js.errorIf(mv.pseudoscope.isPrototypeOf(js.last(this.scopeChain)), 'you cannot shift scope using \'without\' inside a loop!');
         this.scopeChain.pop();
       },
 
       contain: function(key){
-        jQuery(this.node).html(this._lookup(key));
+        jQuery(this.node).html(this.lookup(key));
+      },
+
+      loop: function(keyAlias, valueAlias, conjunction, collectionKey){
+        //todo: support object iteration
+        if(valueAlias === 'in'){
+          collectionKey = conjunction;
+          conjunction = valueAlias;
+          valueAlias = keyAlias;
+          keyAlias = null;
+        }
+        var collection = this.lookup(collectionKey);
+        // todo: return here (and everywhere else) if collection is undefined.  test for this
+
+        var $loopChildren = jQuery(this.node).children();
+/*todo: support and test a missing contents node
+        if($loopChildren.length === 1){
+          $(this.node).append($('<span></span>'));
+          $loopChildren = $(this.node).children();
+        }
+*/
+        js.errorIf(!$loopChildren.length === 2, 'rv looping nodes must contain exactly 2 children - one item template and one results container');
+        var $itemTemplate = $loopChildren.first();
+        this.loopItemTemplates[this.getNodeKey($itemTemplate[0])] = $itemTemplate[0];
+        var $resultsContainer = $loopChildren.last();
+
+        var itemNodes = [];
+        for(var i = 0, length = collection.length; i < length; i++){
+          itemNodes.push($itemTemplate.clone()[0]);
+          var loopItemScope = this.loopItemScopes[this.getNodeKey(js.last(itemNodes))] = {};
+
+          if(typeof keyAlias !== 'undefined'){
+            loopItemScope[keyAlias] = i;
+          }
+
+          loopItemScope[valueAlias] = typeof collection[i] !== 'function' ? collection[i] : (function(original){
+            return function(){
+              return original.apply(collection, arguments);
+            };
+          }(collection[i]));
+        }
+        $resultsContainer.html(itemNodes);
+
+        // add top level item nodes to the update list if they don't have mv attributes
+        var additionalUpdateNodes = typeof $itemTemplate.attr('mv') === 'undefined' ? itemNodes : [];
+        additionalUpdateNodes = additionalUpdateNodes.concat(this._select($resultsContainer[0]).slice(1));
+        this.nodes.push.apply(this.nodes, additionalUpdateNodes);
       },
 
       showIf: function(condition){
-        jQuery(this.node)[this._lookup(condition) ? 'show' : 'hide']();
+        jQuery(this.node)[this.lookup(condition) ? 'show' : 'hide']();
       },
 
       visIf: function(condition){
-        jQuery(this.node).css('visibility', this._lookup(condition) ? 'visible' : 'hidden');
+        jQuery(this.node).css('visibility', this.lookup(condition) ? 'visible' : 'hidden');
       },
 
       attr: function(name, value){
-        name = this._lookup(name);
-        value = this._lookup(value);
+        name = this.lookup(name);
+        value = this.lookup(value);
         js.errorIf(
           !js.among(['string', 'number'], typeof name) ||
           !js.among(['string', 'number'], typeof value),
@@ -72,51 +124,13 @@
       },
 
       attrIf: function(condition, name, value){
-        if(this._lookup(condition)){
-          this.attr(name, value);
+        if(this.lookup(condition)){
+          $(this.node).attr(this.lookup(name), this.lookup(value));
         } else {
-          jQuery(this.node).removeAttr(this._lookup(name));
+          $(this.node).removeAttr(this.lookup(name));
         }
       }
 
-    },
-
-    update: function(root /* , scope1, ..., scopeN */){
-      var baseScopeChain = Array.prototype.slice.call(arguments, 1);
-      /* todo: add support for strings
-      if(typeof scope === 'string'){
-        scope = mv.scopes[scope];
-      }
-      if(typeof root === 'string'){
-        root = mv.nodes[root];
-      }
-      */
-
-      var updateScope = js.create(this.commandScope, {
-        _baseScopeChain: baseScopeChain,
-        _root: root,
-        _scopeChainCache: {},
-        _scopeChainCachedNodes: []
-      });
-
-      this._process(root, updateScope);
-
-      /* todo: support recursionp
-      var nodes = mv.select(node);
-      for(var i = 0, length = nodes.length; i< length; i++){
-        mv._updateSingle(nodes[i], updateScope);
-      }
-
-      // clean up
-      js.util.unique.reset('scopeChain');
-      for(var i = 0, length = updateScope._scopeChainCachedNodes.length; i< length; i++){
-        updateScope._scopeChainCachedNodes[i].scopeChainKey = undefined;
-      }
-      */
-    },
-
-    select: function(node){
-      return [node].concat(Array.prototype.slice.call(node.querySelectorAll('[mv]')));
     },
 
     _matchers: {
@@ -127,33 +141,102 @@
       isNumber: /\d+/
     },
 
-    _process: function(node, updateScope){
-      var directives = node.getAttribute('mv').split(this._matchers.spaceCommaSpace);
+    _select: function(node){
+      return [node].concat(Array.prototype.slice.call(node.querySelectorAll('[mv]')));
+    },
+
+    update: function(root /* , scope1, ..., scopeN */){
+      var baseScopeChain = Array.prototype.slice.call(arguments, 1);
+      /* todo: add support for strings
+      if(typeof scope === 'string'){
+        scope = this.scopes[scope];
+      }
+      if(typeof root === 'string'){
+        root = this.nodes[root];
+      }
+      */
+
+      var nodes = this._select(root);
+      var updateScope = js.create(this, {
+        root: root,
+        baseScopeChain: baseScopeChain,
+        nodes: nodes,
+        scopeChains: {},
+        loopItemScopes: {},
+        loopItemTemplates: {},
+        lookup: this._lookupInScopeChain
+      });
+
+      for(var i = 0; i < nodes.length; i++){
+        this._updateSingle(nodes[i], updateScope);
+      }
+    },
+
+    getNodeKey: function(node){
+      return node.key = node.key || js.util.unique('nodeKey');
+    },
+
+    _updateSingle: function(node, updateScope){
+      //todo: test that you never revisit a node
+      var nodeKey = this.getNodeKey(node);
+      if(updateScope.scopeChains[nodeKey]){
+        // node has already been visited
+        return updateScope.scopeChains[nodeKey];
+      }
+
+      var ancestorScopeChain = updateScope.baseScopeChain;
+      if(node !== updateScope.root){
+        var ancestor = $(node).parent()[0];
+        while(
+          ancestor && // is defined
+          ! ancestor === updateScope.root && // is not root
+          ! ancestor.getAttribute('mv') && // has no mv directives
+          ! this.loopItemScopes[this.getNodeKey(ancestor)] // isnt a loop item
+        ){
+          ancestor = $(ancestor).parent()[0];
+        }
+        if(!ancestor){
+          // node was irrelevant - not a decendant of the root, ignore it
+          return false;
+        }
+        ancestorScopeChain = this._updateSingle(ancestor, updateScope);
+      }
+      if(!ancestorScopeChain || updateScope.loopItemTemplates[this.getNodeKey(node)]){
+        // node is a loop template or ancestor was irrelevant, so this node is irrelevant
+        return false;
+      }
+
       var processingScope = js.create(updateScope, {
         node: node,
-        scopeChain: js.copy(updateScope._baseScopeChain)
+        scopeChain: js.copy(ancestorScopeChain)
+      });
+      if(updateScope.loopItemScopes[nodeKey]){
+        processingScope.scopeChain.push(updateScope.loopItemScopes[nodeKey]);
+      }
+      var directives = (node.getAttribute('mv')||'').split(this._matchers.spaceCommaSpace);
+      directives = js.filter(directives, function(directive){
+        return !!directive;
       });
       for( var i = 0, length = directives.length; i < length; i++ ){
         this._followDirective(processingScope, js.trim(directives[i]).replace(this._matchers.negation, '!').split(this._matchers.space));
       }
+
+      return updateScope.scopeChains[nodeKey] = processingScope.scopeChain;
     },
 
     _followDirective: function(scope, directive){
       var command = directive.shift();
       js.errorIf(!this.commands[command], command+' is not a valid mv command');
       this.commands[command].apply(scope, directive);
-    }
+    },
 
-  };
-
-  mv.commandScope = js.create(mv.commands, {
-    _lookup: function(key){
+    _lookupInScopeChain: function(key){
       var negate;
       if(key[0] === '!'){
         negate = true;
         key = key.slice(1);
       }
-      if (mv._matchers.isString.test(key)) {
+      if (this._matchers.isString.test(key)) {
         value = key.slice(1, key.length-1);
       } else {
         var keys = key.split('.');
@@ -175,9 +258,40 @@
         }
       }
       return negate ? ! value : value;
-    }
-  });
+    },
 
+/*
+    getNodeKey: function(node){
+      var directive, directives = $.trim($node.attr('mv')).split(this.matchers.spaceCommaSpace);
+      for(var which = 0; which < directives.length; which++){
+        directive = $.trim(directives[which]).split(/\s+/);
+        if(directive[0] === 'key'){
+          return directive[1];
+        }
+      };
+    },
+
+    setNodeKey: function($node, key){
+      var directive, directives = $.trim($node.attr('mv')).split(this.matchers.spaceCommaSpace);
+      for(var which = 0; which < directives.length; which++){
+        directive = $.trim(directives[which]).split(/\s+/);
+        if(directive[0] === 'key'){
+          directives[which] = 'key '+key;
+        }
+      };
+      $node.attr('mv', directives.join(', '));
+    },
+
+    _addNodeKey: function($node, nodeKey){
+      return mv.setNodeKey(nodeKey || mv.getNodeNey($node) || js.util.unique('nodeKey'));
+    },
+
+    _addScopeKey: function(scope, scopeKey){
+      return scope._mvScopeKey = scopeKey || scope._mvScopeKey || js.util.unique('scopeKey');
+    }
+*/
+
+  };
 
   mv.integrate = {
     jQuery: function(){
@@ -214,84 +328,13 @@
       scope.tethers = js.filter(tetheredNodeKeys).sort().join(', ');
     },
 
-    _updateNode: function(root, rootScopeChain, $node){
-      if($node[0].scopeChainKey){
-        return mv._scopeChainCache[$node[0].scopeChainKey];
-      }
-
-      var thus = {
-        $node: $node,
-        lookup: mv._scopeChainLookup,
-        scopeChain: js.copy($node[0] === root ? rootScopeChain : mv._updateNode(root, rootScopeChain, mv._getMvAncestor(root, $node)))
-      };
-
-      $.each($.trim(($node.attr('mv')||'')).split(','), function(which, directive){
-        thus.active = true;
-        mv.directive._follow(thus, $.trim(directive).split(/\s+/));
-      });
-      mv._inScopeChainCache.push($node[0]);
-      return mv._scopeChainCache[$node[0].scopeChainKey = js.util.unique('scopeChain')] = thus.scopeChain;
-    },
-
-    getNodeKey: function($node){
-      $node = $($node);
-      js.errorIf($node.length !== 1, 'getNodeKey() cannot operate on arrays multiple nodes');
-      var directive, directives = $.trim($node.attr('mv')).split(this.matchers.spaceCommaSpace);
-      for(var which = 0; which < directives.length; which++){
-        directive = $.trim(directives[which]).split(/\s+/);
-        if(directive[0] === 'key'){
-          return directive[1];
-        }
-      };
-    },
-
-    setNodeKey: function($node, key){
-      $node = $($node);
-      js.errorIf($node.length !== 1, 'getNodeKey() cannot operate on arrays multiple nodes');
-      var directive, directives = $.trim($node.attr('mv')).split(this.matchers.spaceCommaSpace);
-      for(var which = 0; which < directives.length; which++){
-        directive = $.trim(directives[which]).split(/\s+/);
-        if(directive[0] === 'key'){
-          directives[which] = 'key '+key;
-        }
-      };
-      $node.attr('mv', directives.join(', '));
-    },
-
-    _addNodeKey: function($node, nodeKey){
-      return mv.setNodeKey(nodeKey || mv.getNodeNey($node) || js.util.unique('nodeKey'));
-    },
-
-    _addScopeKey: function(scope, scopeKey){
-      return scope._mvScopeKey = scopeKey || scope._mvScopeKey || js.util.unique('scopeKey');
-    },
-
     _getMvAncestor: function(root, $node){
+//todo: test for ancestor being empty, in case the node was deleted in the mean time
       var $ancestor = $node.parent();
       while($ancestor[0] !== root && ! $ancestor.attr('mv')){
         $ancestor = $ancestor.parent();
       }
       return $ancestor;
-    },
-
-    _scopeChainLookup: function(key){
-      var type, value;
-      for(var i = this.scopeChain.length - 1; 0 <= i; i--){
-        // null scopes indicate a shift
-        if(this.scopeChain[i] === null){
-          return;
-        }
-        value = this.scopeChain[i][key];
-        type = typeof value;
-        if(type === 'function'){
-          value = value();
-          type = typeof value;
-          js.errorIf(type === 'undefined', 'mv called a function and got undefined!');
-        }
-        if(type !== 'undefined'){
-          return value;
-        }
-      }
     },
 
     directive: {
@@ -318,6 +361,7 @@
         within: function(key){
           var scope = this.lookup(key);
           if(typeof scope === 'undefined'){ return; };
+  // todo: port and test this
           js.errorIf(typeof scope !== 'object' && typeof scope !== 'array' && typeof scope !== 'function', 'mask commands must receive a namespacing value');
           this.scopeChain.push(scope);
         },
--- tests/tests.js
+++ tests/tests.js
@@ -4,7 +4,7 @@ test('select', function(){
   var grandchild = $('<span mv=""/>')[0];
   var child = $('<p/>').html(grandchild)[0];
   var node = $('<div/>').html(child)[0];
-  var results = mv.select(node);
+  var results = mv._select(node);
   equal(results.length, 2, 'results included 2 nodes');
   ok(js.among(results, node), 'node itself was selected');
   ok(!js.among(results, child), 'non-mv child was not selected');
@@ -26,7 +26,6 @@ test('containing strings', function(){
 
 test('containing variables', function(){
   var node = $('<div mv="contain key"></div>')[0];
-debugger;
   mv.update(node, {key:'value'});
   equal(node.innerHTML, 'value', 'contain directive inserted a string variable');
 });
@@ -124,3 +123,33 @@ test('scope can be shifted within a property', function(){
   equal($(node).html(), 'content', 'key fell through fell through to next higher scope when local key is undefined');
 });
 
+test('works with a missing key alias', function(){/*...*/});
+
+test('can loop across values in an array', function(){
+  var node = $('<div id=\"outter\" mv="loop which item in list"><div id=\"template\" mv="contain item"></div><div id="container"></div></div>')[0];
+  var itemTemplate = $(node).children()[0];
+  var resultsHolder = $(node).children()[1];
+  mv.update(node, {list:['a','b','c']});
+  equal($(node).children().last().children().length, 3, 'results container node contains three child elements');
+  same([
+    $($(resultsHolder).children()[0]).html(),
+    $($(resultsHolder).children()[1]).html(),
+    $($(resultsHolder).children()[2]).html()
+  ], ['a','b','c'], 'children\'s innerHTML is set to array items\' contents');
+  equal($(itemTemplate).html(), '', 'item template was unchanged');
+});
+
+test('can loop across keys in an array', function(){
+  var node = $('<div mv="loop which item in list">'
+               + '<div mv="contain which">'
+               + '</div>'
+               + '<div></div>'
+             + '</div>')[0];
+  var resultsHolder = $(node).children()[1];
+  mv.update(node, {list:['a','b','c']});
+  same([
+    $($(resultsHolder).children()[0]).html(),
+    $($(resultsHolder).children()[1]).html(),
+    $($(resultsHolder).children()[2]).html()
+  ], ['0','1','2'], 'children\'s innerHTML is set to array key\'s contents');
+});
