2300ebb72494db481adb619cea68668f9bc78bc8
Marcus Phillips <github.com@marcusphillips.com>
2011-02-14 02:47:09 -0800

    now inferring scope chains by node path, reworked anchors toward a better memory management

--- react.js
+++ react.js
@@ -36,6 +36,10 @@ remove update context?
       isNumber: /\d+/
     },
 
+    name: function(name, object){
+      this.scopes[name] = object;
+    },
+
     getNodeKey: function(node){
       return (node.reactKey = node.reactKey || js.util.unique('reactNode'));
     },
@@ -73,23 +77,76 @@ remove update context?
       }
     },
 
-    _buildScopeChainFor: function(node, directiveIndex){
+    _buildScopeChainFor: function(node, directiveIndex, options){
+      directiveIndex = directiveIndex || 0;
       // todo: to improve automatic memory management, remove the _scopeChainCache cheat
-      return node._scopeChainCache[directiveIndex];
-    },
-
-    _buildScopeChain: function(scopes){
+      //return node._scopeChainCache[directiveIndex];
       var lastLink;
-      for(var which = 0; which < scopes.length; which++){
-        lastLink = this._addScopeLink(lastLink, scopes[which]);
+      var that = this;
+      var ancestors = $(Array.prototype.reverse.apply($(node).parents())).add(node);
+      for(var whichAncestor = 0; whichAncestor < ancestors.length; whichAncestor++){
+        var eachAncestor = ancestors[whichAncestor];
+        var directives = that._getDirectives(eachAncestor);
+        if(directives.anchored){
+          for(var whichToken = 1; whichToken < directives.anchored.length; whichToken++){
+            var scopeKey = directives.anchored[whichToken];
+            js.errorIf(!that._matchers.isString.test(scopeKey), 'not sure how, but this anchored directive got effed.  it\'s supposed to be auto-generated...');
+            scopeKey = scopeKey.slice(1,scopeKey.length-1);
+            js.errorIf(!that.scopes[scopeKey], 'could not follow anchored directive, nothing found at react.scopes.'+scopeKey);
+            lastLink = that._addScopeLink(lastLink, that.scopes[scopeKey], {type:'anchor'});
+          }
+        }
+        for(var whichDirective = 0; whichDirective < directives.length; whichDirective++){
+          var eachDirective = directives[whichDirective];
+          if(eachAncestor !== node || whichDirective < directiveIndex){
+            if(eachDirective[0] === 'within'){
+              if(!lastLink){ continue; }
+              lastLink = that._addScopeLink(lastLink, lastLink.scope[eachDirective[1]]);
+            }
+            if(eachDirective[0] === 'loop'){
+              if(!lastLink){ continue; }
+              if(eachDirective[1] === 'as'){
+                var loopAliases = {
+                  key: eachDirective.length === 3 ? eachDirective[1] : undefined,
+                  value: js.last(eachDirective)
+                };
+              }
+            }
+            if(eachDirective[0] === 'atKey'){
+              if(!lastLink){ continue; }
+              if(loopAliases){
+                var loopItemScope = {};
+                if(loopAliases.key){
+                  loopItemScope[loopAliases.key] = eachDirective[1];
+                }
+                loopItemScope[loopAliases.value] = new that._Fallthrough(eachDirective[1]);
+                lastLink = this._addScopeLink(lastLink, loopItemScope);
+                delete loopAlias;
+              }else{
+                lastLink = this._addScopeLink(lastLink, lastLink.scope[eachDirective[1]]);
+              }
+            }
+          }
+        }
       }
       return lastLink;
     },
 
-    _addScopeLink: function(link, additionalLink){
+    _buildScopeChain: function(scopes, options){
+      options = options || {};
+      var lastLink = options.prefix;
+      for(var which = 0; which < scopes.length; which++){
+        lastLink = this._addScopeLink(lastLink, scopes[which], options);
+      }
+      return lastLink;
+    },
+
+    _addScopeLink: function(link, additionalLink, options){
+      options = options || {};
       return {
         parent: link,
-        scope: additionalLink
+        scope: additionalLink,
+        type: options.type
       };
     },
 
@@ -118,7 +175,7 @@ remove update context?
         loopItemScopes: {},
         loopItemTemplates: {}
       });
-      var baseScopeChain = this._buildScopeChain([scope] || [options.scope] || options.scopeChain);
+      var baseScopeChain = this._buildScopeChain([scope] || [options.scope] || options.scopeChain, {type:'renderInputs', prefix:this._buildScopeChainFor(root)});
       updateContext.bequeathedScopeChains[this.getNodeKey(root)] = this._updateNodeGivenScopeChain(root, baseScopeChain, updateContext);
 
       for(var i = 0; i < nodes.length; i++){
@@ -172,9 +229,6 @@ remove update context?
       }
 
       var scopeChain = updateContext.bequeathedScopeChains[this.getNodeKey(parent)];
-      if(updateContext.loopItemScopes[nodeKey]){
-        scopeChain = this._addScopeLink(scopeChain, updateContext.loopItemScopes[nodeKey]);
-      }
       updateContext.bequeathedScopeChains[nodeKey] = this._updateNodeGivenScopeChain(node, scopeChain, updateContext);
     },
 
@@ -182,8 +236,8 @@ remove update context?
       var nodeKey = this.getNodeKey(node);
       var directives = this._getDirectives(node);
 
-      var pushScope = function(scope){
-        scopeChain = this._addScopeLink(scopeChain, scope);
+      var pushScope = function(scope, options){
+        scopeChain = this._addScopeLink(scopeChain, scope, options);
       };
 
       for(var i = 0; i < directives.length; i++){
@@ -204,9 +258,35 @@ remove update context?
       var directives = js.map(directiveStrings, function(which, string){
         return js.trim(string).replace(that._matchers.negation, '!').split(that._matchers.space);
       });
-      return js.filter(directives, function(directive){
+      if(directives[0] && directives[0][0] === 'anchored'){
+        var anchored = directives.shift();
+      }
+      directives = js.filter(directives, function(directive){
         return !!directive[0];
       });
+      directives.anchored = anchored;
+      return directives;
+    },
+
+    _setDirectives: function(node, directives){
+      var anchored = directives.anchored;
+      directives = js.filter(directives, function(directive){
+        return !!directive[0];
+      });
+      directives.anchored = anchored;
+      if(directives.anchored){
+        directives.unshift(directives.anchored);
+      }
+      var directiveStrings = js.map(directives, function(which, directive){
+        return directive.join(' ');
+      });
+      node.setAttribute('react', directiveStrings.join(', '));
+    },
+
+    _prependDirective: function(node, directive){
+      var directives = this._getDirectives(node);
+      directives.unshift(directive);
+      this._setDirectives(node, directives);
     },
 
     _followDirective: function(directive, context){
@@ -215,11 +295,27 @@ remove update context?
       this.commands[command].apply(context, directive);
     },
 
-    anchor: function(node, object){
+    // todo: support anchoring to whole scope chains
+    anchor: function(node, object, options){
+      var scopeChain = object ? [object] : options.scopeChain;
       var nodeKey = this.getNodeKey(node);
       this.nodes[nodeKey] = node;
+      var directives = this._getDirectives(node);
+      // todo: any existing anchor cleanup?
+      directives.anchored = ['anchored'];
+      for(var i = 0; i < scopeChain.length; i++){
+        var scopeKey = this.getObjectKey(scopeChain[i]);
+        this.scopes[scopeKey] = object;
+        directives.anchored.push('\''+scopeKey+'\'');
+      }
+      this._setDirectives(node, directives);
+      // todo: these react anchors need refactoring
       object.reactAnchors = object.reactAnchors || {};
       object.reactAnchors[nodeKey] = true;
+    },
+
+    _Fallthrough: function(key){
+      this.key = key;
     }
 
   };
@@ -312,6 +408,12 @@ remove update context?
       return negate ? ! value : value;
     },
 
+    anchored: function(token){
+      js.errorIf(!this._matchers.isString.test(token), 'anchored directive requires a string');
+      token = token.slice(1, length-1);
+      this.pushScope(this.scopes[token], {type:'anchor'});
+    },
+
     within: function(key){
       // todo: port and test this
       // js.errorIf(typeof scope !== 'object' && typeof scope !== 'array' && typeof scope !== 'function', 'mask commands must receive a namespacing value');
@@ -342,10 +444,6 @@ remove update context?
       }
     },
 
-    _Fallthrough: function(key){
-      this.key = key;
-    },
-
     loop: function(as, keyAlias, valueAlias){
       if(valueAlias === undefined){
         valueAlias = keyAlias;
@@ -357,6 +455,7 @@ remove update context?
       var $loopChildren = jQuery(this.node).children();
       js.errorIf($loopChildren.length < 2, 'rv looping nodes must contain at least 2 children - one item template and one results container');
       var $itemTemplate = $loopChildren.first();
+      //js.errorIf(this._getDirectives($itemTemplate[0])[0].join(' ') !== 'item', 'the item template must declare itself with an item directive');
       this.loopItemTemplates[this.getNodeKey($itemTemplate[0])] = $itemTemplate[0];
       var $resultsContainer = $($loopChildren[1]);
       var $resultsContents = $resultsContainer.children();
@@ -366,13 +465,10 @@ remove update context?
 
       var itemNodes = [];
       var itemNode;
+      // todo: support hash collections
       for(var i = 0; i < collection.length; i++){
         // set up a loop item scope to be applied for each item
-        if(!as){
-          // each item in the collection will be the new scope for it's correlative node
-          loopItemScope = collection[i];
-          js.errorIf(!js.among(['function', 'object'], typeof loopItemScope), 'looping without an "as" directive requires that each element of the collection be an object');
-        } else {
+        if(as){
           // a new scope will be created with bindings for valueAlias and optionally for keyAlias
           loopItemScope = {};
           if(keyAlias !== undefined){
@@ -385,6 +481,9 @@ remove update context?
           itemNode = $resultsContents[i];
         } else {
           itemNode = $itemTemplate.clone()[0];
+          // todo: implement bindings as key aliases
+          js.errorIf(this._matchers.space.test(i), 'looping not currently supported over colletions with space-filled keys');
+          this._prependDirective(itemNode, ['atKey', i]);
           this.enqueueNodes([itemNode].concat(Array.prototype.slice.apply(itemNode.querySelectorAll('[react]'))));
         }
         this.loopItemScopes[this.getNodeKey(itemNode)] = loopItemScope;
@@ -394,6 +493,14 @@ remove update context?
       $itemTemplate.hide();
       if(collection.length !== $resultsContents.length){
         $resultsContainer.html(itemNodes);
+      }
+    },
+
+    atKey: function(key){
+      if(this.loopItemScopes[this.getNodeKey(this.node)]){
+        this.pushScope(this.loopItemScopes[this.getNodeKey(this.node)]);
+      }else{
+        this.within(key);
       }
     },
 
