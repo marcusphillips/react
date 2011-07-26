3c224e6837822e8f511f346f41b817fb14f49f1f
Marcus Phillips <github.com@marcusphillips.com>
2011-07-25 20:31:06 -0700

    new test to cover dangerous case for progressive rendering

--- tests/tests.js
+++ tests/tests.js
@@ -393,6 +393,29 @@ test('loops can be changed()', function(){
   testItems(node, data);
 });
 
+test('after a lookup failure on leaf object that falls through to parent level in scope chain, an update to the key on the leaf scope removes listener on parent scope', function(){
+  var node = $('\
+    <div react="within child">\
+      <div id="child" react="contain prop"></div>\
+    <span></span></div>\
+  ')[0];
+  var object = {
+    prop: 'parent prop',
+    child: {
+    }
+  };
+
+  react.anchor(node, object);
+  react.update(node);
+  equal($('#child', node).html(), 'parent prop');
+  react.set(object.child, 'prop', 'child prop');
+  equal($('#child', node).html(), 'child prop');
+  object.child.prop = 'changed child prop';
+  react.set(object, 'prop', 'changed parent prop');
+  equal($('#child', node).html(), 'child prop');
+  react.changed(object.child, 'prop');
+  equal($('#child', node).html(), 'changed child prop');
+});
 
 /*
  * withinEach
